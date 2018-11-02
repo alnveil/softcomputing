e5b974f4ddaac4f445278d5c82712781d3818f88
krisnasudiatmika <39329981+krisnasudiatmika@users.noreply.github.com>
2018-11-02 08:55:28 +0800

    Update kuliahperceptron.py

--- kuliahperceptron.py
+++ kuliahperceptron.py
@@ -1,7 +1,7 @@
 import  numpy as np
 
 class Perceptron(object):
-    """Implements a perceptron network"""
+
 
     def __init__(self, input_size, lr=1, epochs=100):
         self.W = np.zeros(input_size + 1)
